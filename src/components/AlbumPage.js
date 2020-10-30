9f0a514b75d5b3ab324a0491dfabfc8d8a6eb9a0
Andrew Choi <adc1225.dev@gmail.com>
2020-10-30 14:39:58 -0600

    functionality for artist page working - minus follow button

--- src/components/AlbumPage.js
+++ src/components/AlbumPage.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import '../css/album-page.css';
 import Nav from './Nav';
 // import {getAlbums} from '../store/actions/albums';
-import {getOneAlbum} from '../store/actions/album';
+import {getOneAlbum, setCurrent} from '../store/actions/album';
 
 const AlbumPage = (props) => {
+    const [currentTrack, setCurrentTrack] = useState('');
     const current = useSelector((state) => state.album.current)
 
     const dispatch = useDispatch();
@@ -26,6 +30,7 @@ const AlbumPage = (props) => {
     const album = current.album;
     const tracks = current.album.Tracks;
     const artist = current.album.User;
+    const trackTest = tracks[0].trackUrl;
 
     return (
         <>
@@ -34,30 +39,44 @@ const AlbumPage = (props) => {
 
                 <div className="album-page">
                     <div className="album-page__header-holder">
-                    <h1 className="album-page__header">{artist.artistName}</h1>
+                    <h1 className="album-page__header">{album.title}</h1>
                     </div>
 
                     <div className="album-page__content-holder">
                         <div className="album-page__left-content">
-                            <div className="album-header">
+                            {/* <div className="album-header">
                                 {album.title}
-                            </div>
+                            </div> */}
 
-                            <div>
+                            <div className="artist-name-holder">
                                 by {artist.artistName}
                             </div>
+                            
+                            <div className="audio-player-holder">
+                                <AudioPlayer
+                                    autoPlay={false}
+                                    volume={0.5}
+                                    src={currentTrack}
+                                    onPlay={e => console.log("onPlay")}
+                                    showDownloadProgress={true}
+                                />
+                            </div>
 
-                            {tracks.map((track) => (
-                                <div key={track.id} className="audio-player">
-                                    {track.title}
+
+                            {tracks.map((track, i) => (
+                                <div key={track.id} className="track-holder">
+                                    <span>{i+1}. </span> 
+                                    <button onClick={() => setCurrentTrack(track.trackUrl)} className="track">
+                                        {track.title}
+                                    </button>
                                 </div>
                             ))}
 
                         </div>
 
                         <div className="album-page__middle-content">
-                            <div className="album-cover">
-                                <img src={album.imgUrl} />
+                            <div className="album-cover-holder">
+                                <img className="album-cover" src={album.imgUrl} />
                             </div>
                             <div className="description">
                                 {album.description}
@@ -68,13 +87,11 @@ const AlbumPage = (props) => {
                        
                             <img className="artist-photo" src={artist.imgUrl} />
                             
-                            <div>
+                            <div className="artist-name">
                                 {artist.artistName}
                             </div>
 
-                            <div className="follow-btn">
-
-                            </div>
+                            <button className="follow-btn">Follow</button>
 
                             <div className="bio">
                                 {artist.bio}
